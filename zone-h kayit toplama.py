35ba85a7989e9954bddcd01971d05a1348f78866
Cüneyt Tanrısever <55285425+cuneyttanrisever@users.noreply.github.com>
2021-08-08 16:33:23 +0300

    Update zone-h kayit toplama.py

--- zone-h kayit toplama.py
+++ zone-h kayit toplama.py
@@ -11,7 +11,7 @@ print ("""
 #   sadece cookielerden ZH ve PHPSESSID degerleri alip             #
 #   dosya icindeki yerleri bulup ZH= ile PHPSESSID editleyiniz.    #
 #################################################################### """)
-Hackeradi=input("Hacker adı =  ")
+hackeradi=input("Hacker adı =  ")
 d = input("Kac sayfa zone cekmek istiyorsunuz =  ")
 yaz = open("zoneler.txt", "w+")
 yaz.close()
@@ -19,11 +19,12 @@ ara="/mirror/id/(.*?)\"\>mirror"
 zoneler=[]
 for i in range(int(d)):
     say1=  i+1
-    urlc= "http://zone-h.org/archive/notifier=%s/page=%s" %(Hackeradi,say1)
+    urlc= "http://zone-h.org/archive/notifier={}/page={}".format(hackeradi,say1)
+    #print(urlc)
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.28 Safari/537.36' }
     rq=requests.session()
     rq.headers.update(headers)
-    cookies= {'ZHE':'7eda371aaa927ceb7b686a1813d0ae7f', 'PHPSESSID':'mbtdq37armftau3cdspte4ngf7'}
+    cookies= {'ZHE':'850593b3d8751d9089d3201df8423338', 'PHPSESSID':'h2svhqp75rv2q8o1qhmrof9u74'}
     rq = requests.get(urlc, cookies=cookies)
     dex=rq.content.decode('utf-8')
     pattern  = re.compile(ara)
@@ -40,3 +41,4 @@ for  i in zoneler:
     yaz = open("zoneler.txt", "a")
     yaz.write(i+"\n")
     yaz.close()
+
